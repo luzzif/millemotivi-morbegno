98bbbd20813cb06fbf186c86b27838a1ad0043e0
luzzif <federico.luzzi@mondora.com>
2019-10-29 19:59:32 +0100

    feat: insert proper card text

--- src/components/what-do-we-do/index.jsx
+++ src/components/what-do-we-do/index.jsx
@@ -4,6 +4,27 @@ import { Grid } from "../grid";
 import { useStaticQuery, graphql } from "gatsby";
 import { Root } from "./styled";
 
+const cards = [
+    {
+        key: "gadget",
+        title: "Gadget",
+        description:
+            "Cerchi la soluzione vincente per valorizzare la tua azienda? Millemotivi fa al caso tuo! A disposizione per voi una vasta scelta di prodotti da personalizzare, dalla classica penna all'accessorio di ultima generazione."
+    },
+    {
+        key: "wearing",
+        title: "Abbigliamento",
+        description:
+            "Fai parte di una squadra sportiva, sei il titolare di un'azienda o semplicemente hai un evento da ricordare? Scegli il capo d'abbigliamento più adatto a te, la grafica che più ti piace e il gioco è fatto."
+    },
+    {
+        key: "digital-print",
+        title: "Stampa digitale",
+        description:
+            "Dalla stampa di manifesti, banner ed etichette, alla personalizzazione di vetrine, insegne e automezzi. Questi sono solo alcuni dei servizi che offriamo; vi seguiamo dalla realizzazione della grafica alla scelta del supporto più adatto a te."
+    }
+];
+
 export const WhatWeCanDoForYou = () => {
     const { placeholderImage } = useStaticQuery(graphql`
         query {
@@ -30,27 +51,15 @@ export const WhatWeCanDoForYou = () => {
                 <h1>Cosa facciamo</h1>
             </Grid>
             <Grid item container spacingRatio={8} xs={11} md={8}>
-                <Grid item xs={12} sm={4}>
-                    <Card
-                        title="Lorem ipsum dolor sit amet"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip"
-                        image={placeholderImage.childImageSharp.fluid}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Card
-                        title="Lorem ipsum dolor sit amet"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip"
-                        image={placeholderImage.childImageSharp.fluid}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Card
-                        title="Lorem ipsum dolor sit amet"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip"
-                        image={placeholderImage.childImageSharp.fluid}
-                    />
-                </Grid>
+                {cards.map(card => (
+                    <Grid item xs={12} sm={4} key={card.key}>
+                        <Card
+                            title={card.title}
+                            description={card.description}
+                            image={placeholderImage.childImageSharp.fluid}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </Root>
     );
