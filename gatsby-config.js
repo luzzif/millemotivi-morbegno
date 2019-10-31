ca28c7651bb8e0d353d0f7721b5e8aa6b66ba4e9
luzzif <federico.luzzi@mondora.com>
2019-10-31 11:54:17 +0100

    fix: explicitly add styled components gatsby plugin to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
             options: {
                 pathToConfigModule: `src/utils/typography`
             }
-        }
+        },
+        "gatsby-plugin-styled-components"
     ]
 };
