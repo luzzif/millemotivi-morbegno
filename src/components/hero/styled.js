fe68d5b82b834bec5b262fab2517641b39bbbc4d
luzzif <federico.luzzi@mondora.com>
2019-11-01 22:49:28 +0100

    fix: reduce hero height

--- src/components/hero/styled.js
+++ src/components/hero/styled.js
@@ -4,7 +4,7 @@ import Img from "gatsby-image";
 
 export const RootGrid = styled(Grid)`
     width: 100%;
-    height: 100vh;
+    height: 80vh;
     position: relative;
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 `;
