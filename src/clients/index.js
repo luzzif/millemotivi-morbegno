f9c100292952b3ef9edee8660222178ac4d5021e
luzzif <federico.luzzi@mondora.com>
2019-11-02 19:06:04 +0100

    fix: hardcoded forwarder url

--- src/clients/index.js
+++ src/clients/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const mailForwarderClient = axios.create({
-    baseURL: process.env.REACT_APP_MAIL_FORWARDER_URL
+    baseURL: "https://morbegnoprint-mail-forwarder.herokuapp.com/"
 });
